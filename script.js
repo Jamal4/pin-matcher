13c3794f29f957277ae7de913d44b57133fa4239
Jamal4 <ujamal4@gmail.com>
2020-07-28 10:50:49 +0800

    random number generation complete

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 // task #1 to hide 'notify-section' done at CSS file
 
 // task #2 generate random 4 digit pin
+document
+  .getElementById("random_generator")
+  .addEventListener("click", function () {
+    let numberGenerated = document.getElementById("random_number");
+    let randomNumber = Math.floor(1000 + Math.random() * 9000);
+    numberGenerated = randomNumber;
+    document.getElementById("random_number").value = randomNumber;
+  });
